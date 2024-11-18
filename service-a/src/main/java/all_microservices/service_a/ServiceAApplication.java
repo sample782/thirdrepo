95d3b999402746b3139d6dc2dd4806fb81cfb161
sample782 <gitgit782@gmail.com>
2024-11-18 09:43:44 +0530

    initial commit

---
+++ service-a/src/main/java/all_microservices/service_a/ServiceAApplication.java
@@ -0,0 +1,25 @@
+package all_microservices.service_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ServiceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAApplication.class, args);
+	}
+
+}
+
+//http://localhost:8081/api/hello
+
+@RestController
+class ServiceAController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from Service A";
+    }
+}
